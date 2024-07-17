e2258f7c9f03c4347fc725453a52e49a143bd6b9
Bob Vandevliet <info@bvandevliet.nl>
2024-07-17 15:45:42 +0200

    Do not end all logs with a dot.

--- root/app/run.sh
+++ root/app/run.sh
@@ -30,7 +30,7 @@ do
     ipv4='^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$'
     ipv6='^(([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)|([0-9a-fA-F]{1,4}:){1,6}:|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}))?((25[0-5]|(2[0-4]|1?\d)?\d)\.){3}(25[0-5]|(2[0-4]|1?\d)?\d)|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1?\d)?\d)\.){3}(25[0-5]|(2[0-4]|1?\d)?\d))$'
     if ! [[ $CurrentIP =~ $ipv4 || $CurrentIP =~ $ipv6 ]]; then
-        echo "$(date +'%Y-%m-%d %T'): Error getting IP address, response was: $CurrentIP."
+        echo "$(date +'%Y-%m-%d %T'): Error getting IP address, response was: $CurrentIP"
         sleep 30
         continue
     fi
@@ -52,12 +52,12 @@ do
             if [ $? == 0 ]; then
 
                 # IP has been set for the current root domain
-                echo "$(date +'%Y-%m-%d %T'): Set $ROOTRECORD.$DOMAIN -> $CurrentIP."
+                echo "$(date +'%Y-%m-%d %T'): Set $ROOTRECORD.$DOMAIN -> $CurrentIP"
             else
                 Success=false
 
                 # Failed to set IP for the current root domain
-                echo "$(date +'%Y-%m-%d %T'): Failed to set $ROOTRECORD.$DOMAIN -> $CurrentIP."
+                echo "$(date +'%Y-%m-%d %T'): Failed to set $ROOTRECORD.$DOMAIN -> $CurrentIP"
             fi
         done
 
